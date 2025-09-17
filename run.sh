024c83da61c995c582070d7cb37e3026230d2301
FestusNtala <cse24-152@thuto.bac.ac.bw>
2025-09-18 00:31:53 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Setting up Banking System..."
+cd BankingSystem
+javac model/*.java service/*.java ui/*.java
+echo "Compilation complete! Starting application..."
+java bankingsystem.ui.BankingSystem
