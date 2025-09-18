c17cd0d2d5444e1be4fff2758e210b349dbf6dcb
FestusNtala <cse24-152@thuto.bac.ac.bw>
2025-09-18 19:44:17 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-echo '#!/bin/bash
-echo "Setting up Banking System..."
-cd BankingSystem
-javac model/*.java service/*.java ui/*.java
-echo "Compilation complete! Starting application..."
-java bankingsystem.ui.BankingSystem' > run.sh
